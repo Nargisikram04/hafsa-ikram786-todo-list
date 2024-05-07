93e696fefe8eebc3038b4233a2df7698f009be41
Nargis Ikram <169091156+Nargisikram04@users.noreply.github.com>
2024-05-06 22:08:01 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let todoList: string []= [];
+let conditions = true;
+
+console.log(chalk.red("<<<<==================<<<<>>>>=====================>>>>"));
+console.log(chalk.blue(" \n \t Welcome to Hafsa's Todo-list Aplication\t\n "));
+console.log(chalk.red("<<<<==================<<<<>>>>======================>>>>"));
+
+ while(conditions){
+     let addTask = await inquirer.prompt([
+         {
+             name: "task",
+             type: "input",
+             message: chalk.blue("Enter your New task"),
+         }
+     ]);
+     todoList.push(addTask.task);
+     console.log(chalk.yellow(`${addTask.task} Task added in Todo-list successfully`));
+    
+     let addMoreTask = await inquirer.prompt([
+         {
+             name: "addMore",
+             type: "confirm",
+             message:chalk.green( "Do you want to add more task ?"),
+             default: "false"
+         }
+     ]);
+     conditions = addMoreTask.addMore
+ }
+ console.log("your updated Todo-List ", todoList);
